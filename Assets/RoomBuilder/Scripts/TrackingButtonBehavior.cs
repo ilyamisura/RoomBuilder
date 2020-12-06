98e37679e825fe4931d794260f9321b29e8f057b
Ilija Misjura <ilyamisura1@gmail.com>
2020-12-06 14:08:28 +0200

    add furniture models pack from assets store

---
+++ Assets/RoomBuilder/Scripts/TrackingButtonBehavior.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrackingButtonBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void OnTrackingButtonClick()
+    {
+        SceneManager.LoadScene("MarkersTracking");
+    }
+}
